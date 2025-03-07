bdb7ed48f2608a63bc28ca234cdc8c970c4bf129
alliebowen <122921430+alliebowen@users.noreply.github.com>
2025-03-07 14:12:28 -0500

    Create parseConfig.js

---
+++ src/parseConfig.js
@@ -0,0 +1,7 @@
+import Parse from "parse";
+
+// Initialize Parse with Back4App credentials
+Parse.initialize("3p7vDZey2Ai7Up1CmOXQumEnBaSPk8J0ZLpgUjwX", "Q2WPhWLxJjRHPRu60buXHg6bfEA4WYWk3wqVUFoX");
+Parse.serverURL = "https://parseapi.back4app.com/";
+
+export default Parse;
